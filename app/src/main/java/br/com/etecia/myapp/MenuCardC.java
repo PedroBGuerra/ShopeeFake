2a85515e75ecf1e6fb179800845453d572584c22
louisesimao <louisemichaelle28@gmail.com>
2025-05-29 21:48:57 -0300

    seila

--- app/src/main/java/br/com/etecia/myapp/MenuCardC.java
+++ app/src/main/java/br/com/etecia/myapp/MenuCardC.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MenuCards extends AppCompatActivity {
+public class MenuCardC extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.menucards_layout);
+        setContentView(R.layout.menucardc_layout);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
